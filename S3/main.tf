5a3b9bec3c60613e8d4bc99b1308c573de1be4ef
saivinay0407 <vinaysaiv2000@gmail.com>
2026-07-29 09:51:09 +0530

    s3 bucket

--- S3/main.tf
+++ S3/main.tf
@@ -11,11 +11,14 @@ resource "aws_s3_bucket" "myBucket" {
     region ="us-east-1"
     bucket ="venkatasaivinay-s3-bucket"
     # acl    = "private"
-    versioning {
-        enabled =true
-    }
     tags ={
         Name="venkatasaivinay-s3-bucket"
         environment="Dev"
     }
+}
+resource "aws_s3_bucket_versioning" "versioning" {
+        bucket = aws_s3_bucket.myBucket.id
+        versioning_configuration {
+            status ="Enabled"
+        }
 }
